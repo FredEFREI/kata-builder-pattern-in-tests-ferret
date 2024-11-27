632a130ba972c52048b859ca3e9b3a7f01419503
FredEFREI <116546099+FredEFREI@users.noreply.github.com>
2024-11-27 09:44:32 +0100

    Initialisation
    Fork kata and remove unused languages

--- java/src/main/java/info/dmerej/Address.java
+++
@@ -1,4 +0,0 @@
-package info.dmerej;
-
-public record Address(String line1, String line2, String city, String zipCode, String country) {
-}
--- java/src/main/java/info/dmerej/User.java
+++
@@ -1,4 +0,0 @@
-package info.dmerej;
-
-public record User(String name, String email, int age, boolean verified, Address addres) {
-}
